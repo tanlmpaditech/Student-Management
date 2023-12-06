bef084f62355aca58cd182065d4de458df8d7e31
tanlmpaditech <manhtanluu@gmail.com>
2023-12-06 17:03:43 +0700

    fixed

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,15 +1,14 @@
 import db from '../models/index';
 
 let getHomePage = async (req, res) => {
-    // try{
-    //     let data = await db.Student.findAll();
-    //     return res.render("homepage.ejs", {
-    //         data: JSON.stringify(data)
-    //     });
-    // } catch(err) {
-    //     console.log(err);
-    // }
-    res.send('Hello, World')
+    try{
+        let data = await db.Student.findAll();
+        return res.render("homepage.ejs", {
+            data: JSON.stringify(data)
+        });
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 // let getCRUD = async (req, res) => {
